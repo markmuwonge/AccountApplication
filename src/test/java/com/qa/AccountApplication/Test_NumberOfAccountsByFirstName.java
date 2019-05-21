57101e71814554d6cd3fbde2024e8e49f282c42b
markmuwonge <contactmarkmuwonge@gmail.com>
2019-05-21 16:20:07 +0100

    Number Of Accounts By First Name - Test 2 - Fail
    Should be case insensitive

--- src/test/java/com/qa/AccountApplication/Test_NumberOfAccountsByFirstName.java
+++ src/test/java/com/qa/AccountApplication/Test_NumberOfAccountsByFirstName.java
@@ -6,11 +6,18 @@ import junit.framework.TestCase;
 
 public class Test_NumberOfAccountsByFirstName extends TestCase {
 	
+//	@Test
+//	public void test1()
+//	{
+//		Service service = new Service();
+//		assertEquals(1, service.numberOfAccountsByFirstName("Mark"));
+//	}
+	
 	@Test
-	public void test1()
+	public void test2()
 	{
 		Service service = new Service();
-		assertEquals(1, service.numberOfAccountsByFirstName("Mark"));
+		assertEquals(1, service.numberOfAccountsByFirstName("mark"));
 	}
 	
 
